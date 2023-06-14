f32f48ae6809c65a40ee1d8d578cb01ffb1053d9
mqhele mguni <mqhelemguni19@gmail.com>
2023-06-14 04:02:29 +0200

    second commit

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -10,6 +10,7 @@ const CarItem = (props) => {
           style={styles.image}
           />
         <View style={styles.titles}>
+          <Text style={styles.title}>Design by Mqhele</Text>
           <Text style={styles.title}>Model S</Text>
           <Text style={styles.subtitle}>Starting at $69,420</Text>
         </View>
