ae37be3422ef3e36578a3c68cace6a7f9da0b97f
mqhele mguni <mqhelemguni19@gmail.com>
2023-06-18 04:35:22 +0200

    second commit

---
+++ components/StyledButton/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const StyledButton = (props) => {
+	return (
+	<View>
+	<Text>Just a component</Text>
+	</View>
+	);
+	};
+	
+	export default StyledButton ;
